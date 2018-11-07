8996436296d121194e71187fb0b5e77a57d093bc
Jonathan Levin <jon.levin@inceptionxr.com>
2018-11-07 11:16:01 +0200

    add assets

---
+++ Assets/Plugins/gamekit/Examples/Old/Dinos/GroundPlaneController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Ludiq;
+using UnityEngine;
+
+public class GroundPlaneController : MonoBehaviour
+{
+	[SerializeField] private GameObject _groundPlane;
+
+    [SerializeField] private GameObject _startPrefabExp;
+    [SerializeField] private Vector3 _startExpLocalPos;
+
+    private void Start()
+    {
+        if (_startPrefabExp)
+        {
+            SetExpFromPrefab(_startPrefabExp, _startExpLocalPos);
+        }
+    }
+
+    #region Set Experience object (to appear on next click)
+    public void SetExpFromPrefab(GameObject exp)
+    {
+        var instance = Instantiate(exp);
+        SetExpFromSceneObj(instance);
+    }
+
+    public void SetExpFromPrefab(GameObject exp, Vector3 localPos)
+    {
+        var instance = Instantiate(exp);
+        SetExpFromSceneObj(instance, localPos);
+    }
+
+    public void SetExpFromSceneObj(GameObject exp, Vector3 localPos)
+	{
+        SetExpFromSceneObj(exp);
+		exp.transform.localPosition = localPos;
+	}
+
+	public void SetExpFromSceneObj(GameObject exp)
+	{
+		RemoveChildren(_groundPlane.transform);
+
+		exp.transform.parent = _groundPlane.transform;
+		exp.SetActive(true);
+	}
+    #endregion
+
+    public void PlaceGroundPlane(Vector3 worldPos)
+    {
+        _groundPlane.transform.position = worldPos;
+    }
+
+	private void RemoveChildren(Transform parent)
+	{
+		// NOTE: dont need reverse traversal since Destroy is not immediate.
+
+		for (var i = 0; i < parent.childCount; i++)
+		{
+			var child = parent.GetChild(i).gameObject;
+			Destroy(child);
+		}
+	}
+
+}
---
+++ Assets/Plugins/gamekit/Examples/Old/Dinos/TestGroundPlane.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGroundPlane : MonoBehaviour
+{
+	[SerializeField] private GroundPlaneController controller;
+	[SerializeField] private GameObject[] stuff;
+	private int i = 0;
+	private GameObject NextThing
+	{
+		get
+		{
+			if (stuff.Length == 0)
+			{
+				Debug.LogError("No stuff defined!");
+				return null;
+			}
+			i++; 				// increase index 
+			i %= stuff.Length; 	// wrap around
+			return stuff[i];
+		}
+	} 
+
+	public void OnClick()
+	{
+		var copy = Instantiate(NextThing);
+		controller.SetExpFromSceneObj(copy, Vector3.zero);
+	}
+}
